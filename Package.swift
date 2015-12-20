51575f20db35ff0ffddf51355a3bbda8bff2b711
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-20 07:00:47 -0800

    fix 2015-12-18 toolchain SPM bug

--- Package.swift
+++ Package.swift
@@ -4,6 +4,8 @@ let package = Package(
 	name: "WebSocket",
 	dependencies: [
 		.Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 1),
-		.Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 1)
+		.Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 1),
+		.Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 1),
+		.Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 1)
 	]
 )
