85dc3478d3315caa9c3fff043f6464c015a9d0bf
antonmes <antonmes@users.noreply.github.com>
2016-03-16 09:58:55 +0300

    Swift 3 renaming

--- Package.swift
+++ Package.swift
@@ -1,24 +1,14 @@
 import PackageDescription
 
-#if os(OSX)
-    let openSSLURL = "https://github.com/Zewo/COpenSSL-OSX.git"
-#else
-    let openSSLURL = "https://github.com/Zewo/COpenSSL.git"
-#endif
-
 let package = Package(
 	name: "WebSocket",
 	dependencies: [
-		.Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 3),
-		.Package(url: "https://github.com/Zewo/HTTPClient.git", majorVersion: 0, minor: 3),
-		.Package(url: "https://github.com/Zewo/HTTPSClient.git", majorVersion: 0, minor: 3),
-		.Package(url: "https://github.com/Zewo/Event.git", majorVersion: 0, minor: 2),
-		.Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 2),
-		.Package(url: "https://github.com/Zewo/OpenSSL.git", majorVersion: 0, minor: 2),
-		.Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 2),
-		.Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 2),
-		.Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 2),
-		.Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 2),
-		.Package(url: openSSLURL, majorVersion: 0, minor: 2)
+		.Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 4),
+		.Package(url: "https://github.com/Zewo/HTTPClient.git", majorVersion: 0, minor: 4),
+		.Package(url: "https://github.com/Zewo/HTTPSClient.git", majorVersion: 0, minor: 4),
+		.Package(url: "https://github.com/Zewo/Event.git", majorVersion: 0, minor: 4),
+		.Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 4),
+		.Package(url: "https://github.com/Zewo/OpenSSL.git", majorVersion: 0, minor: 4),
+		.Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 4),
 	]
 )
