67d99b4949531b1d84e3fbb7db6e53e86fe37823
Tomohisa Takaoka <tomohisa@jtechs.com>
2016-05-06 16:20:26 +0900

    Updated for 05-03 Snapshot

--- Package.swift
+++ Package.swift
@@ -3,12 +3,10 @@ import PackageDescription
 let package = Package(
 	name: "WebSocket",
 	dependencies: [
-		.Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 5),
-		.Package(url: "https://github.com/VeniceX/HTTPClient.git", majorVersion: 0, minor: 5),
-		.Package(url: "https://github.com/VeniceX/HTTPSClient.git", majorVersion: 0, minor: 5),
+		.Package(url: "https://github.com/tomohisa/HTTP.git", majorVersion: 0, minor: 6),
+		.Package(url: "https://github.com/tomohisa/HTTPClient.git", majorVersion: 0, minor: 6),
+		.Package(url: "https://github.com/tomohisa/HTTPSClient.git", majorVersion: 0, minor: 7),
 		.Package(url: "https://github.com/Zewo/Event.git", majorVersion: 0, minor: 5),
-		.Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 5),
-		.Package(url: "https://github.com/Zewo/OpenSSL.git", majorVersion: 0, minor: 5),
-		.Package(url: "https://github.com/VeniceX/Venice.git", majorVersion: 0, minor: 5),
+		.Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 7),
 	]
 )
