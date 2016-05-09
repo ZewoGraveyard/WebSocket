eb75cf0af6e9d1662009d81f6a6d047970701bb3
Tomohisa Takaoka <tomohisa@jtechs.com>
2016-05-09 17:59:54 +0900

    fixed text style

--- Tests/WebSocket/FrameTests.swift
+++ Tests/WebSocket/FrameTests.swift
@@ -6,7 +6,7 @@ class FrameTests: XCTestCase {
         XCTAssert(2 + 2 == 4, "Something is severely wrong here.")
     }
     func testMaskPong() {
-        let maskKey = Data([0x39,0xfa,0xab,0x35])
+        let maskKey = Data([0x39, 0xfa, 0xab, 0x35])
         let frame = Frame(opCode: .Pong, data: [], maskKey: maskKey)
         let data = frame.getData()
         var pass = Data()
@@ -17,7 +17,7 @@ class FrameTests: XCTestCase {
         XCTAssert(data==pass, "Frame does not match with Pong case")
     }
     func testMaskText() {
-      let maskKey = Data([0x39,0xfa,0xab,0x35])
+      let maskKey = Data([0x39, 0xfa, 0xab, 0x35])
       let frame = Frame(opCode: .Text, data: "Hello".data, maskKey: maskKey)
       let data = frame.getData()
       let pass = Data([0x81, 0x85, 0x39, 0xfa, 0xab, 0x35, 0x71, 0x9f, 0xc7, 0x59, 0x56])
