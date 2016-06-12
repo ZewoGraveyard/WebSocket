5b4c3101ef5c03ab7fa7ce907493e064d2b65e18
Alex Studnička <astudnicka@gmail.com>
2016-06-12 11:42:48 +0200

    Bump dependencies

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "WebSocket",
     dependencies: [
         .Package(url: "https://github.com/Zewo/Event.git",  majorVersion: 0, minor: 5),
-        .Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 8),
         .Package(url: "https://github.com/Zewo/POSIX.git", majorVersion: 0, minor: 5),
     ]
 )
--- README.md
+++ README.md
@@ -15,7 +15,7 @@ import PackageDescription
 
 let package = Package(
     dependencies: [
-        .Package(url: "https://github.com/Zewo/WebSocket.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/Zewo/WebSocket.git", majorVersion: 0, minor: 8),
     ]
 )
 ```
