f94c39e0416fff92760c21037187cb87ca64404f
badim <goloveychuk@mail.ru>
2016-06-14 13:38:51 +0300

    3.0-preview-1

--- Tests/WebSocket/FrameTests.swift
+++ Tests/WebSocket/FrameTests.swift
@@ -6,7 +6,7 @@ class FrameTests: XCTestCase {
         let maskKey = Data([0x39, 0xfa, 0xab, 0x35])
         let frame = Frame(opCode: .pong, data: [], maskKey: maskKey)
         let data = frame.data
-        var pass = Data()
+        var pass = Data([])
         pass.append(0b10001010)
         pass.append(0b10000000)
         pass += maskKey
