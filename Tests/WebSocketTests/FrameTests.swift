be6e90a029225a3ff1fc0ce9402f4e7955123b54
Alex Tran Qui <alex@katalysis.io>
2016-09-22 22:18:38 +0200

    Fixed tests

--- Tests/WebSocketTests/FrameTests.swift
+++ Tests/WebSocketTests/FrameTests.swift
@@ -4,19 +4,19 @@ import XCTest
 class FrameTests: XCTestCase {
     func testMaskPong() {
         let maskKey = Data([0x39, 0xfa, 0xab, 0x35])
-        let frame = Frame(opCode: .pong, data: [], maskKey: maskKey)
+        let frame = Frame(opCode: .pong, data: Data(), maskKey: maskKey)
         let data = frame.data
         var pass = Data()
         pass.append(0b10001010)
         pass.append(0b10000000)
-        pass += maskKey
-        pass += []
+        pass = pass + maskKey
+        pass = pass + Data()
         XCTAssert(data == pass, "Frame does not match with pong case")
     }
 
     func testMaskText() {
       let maskKey = Data([0x39, 0xfa, 0xab, 0x35])
-      let frame = Frame(opCode: .text, data: "Hello", maskKey: maskKey)
+      let frame = Frame(opCode: .text, data: Data("Hello"), maskKey: maskKey)
       let data = frame.data
       let pass = Data([0x81, 0x85, 0x39, 0xfa, 0xab, 0x35, 0x71, 0x9f, 0xc7, 0x59, 0x56])
       XCTAssert(data == pass, "Frame does not match with text case")
