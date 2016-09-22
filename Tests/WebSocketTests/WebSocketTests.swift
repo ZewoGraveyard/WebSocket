f1293a24a602be7483f49ac43793a7dc626c1c86
Alex Tran Qui <alex@katalysis.io>
2016-09-22 22:14:04 +0200

    Moved Tests directory to new naming convention

---
+++ Tests/WebSocketTests/WebSocketTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import WebSocket
+
+class WebSocketTests: XCTestCase {
+    func testReality() {
+        XCTAssert(2 + 2 == 4, "Something is severely wrong here.")
+    }
+}
+
+extension WebSocketTests {
+    static var allTests: [(String, (WebSocketTests) -> () throws -> Void)] {
+        return [
+           ("testReality", testReality),
+        ]
+    }
+}
