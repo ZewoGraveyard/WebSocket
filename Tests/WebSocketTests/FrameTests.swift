65407a54006190d021928f06a859d1976f6542e3
Dan Appel <dan.appel00@gmail.com>
2016-10-04 16:42:38 -0700

    Update tests, fix indentation

--- Tests/WebSocketTests/FrameTests.swift
+++ Tests/WebSocketTests/FrameTests.swift
@@ -1,13 +1,13 @@
 import XCTest
-import Core
+import Axis
 @testable import WebSocket
 
 public class FrameTests : XCTestCase {
     func testMaskPong() {
-        let maskKey = Data([0x39, 0xfa, 0xab, 0x35])
-        let frame = Frame(opCode: .pong, data: Data(), maskKey: maskKey)
+        let maskKey = Buffer([0x39, 0xfa, 0xab, 0x35])
+        let frame = Frame(opCode: .pong, data: Buffer(), maskKey: maskKey)
         let data = frame.data
-        var pass = Data()
+        var pass = Buffer()
         pass.append(0b10001010)
         pass.append(0b10000000)
         pass.append(maskKey)
@@ -15,11 +15,11 @@ public class FrameTests : XCTestCase {
     }
 
     func testMaskText() {
-      let maskKey = Data([0x39, 0xfa, 0xab, 0x35])
-      let frame = Frame(opCode: .text, data: "Hello", maskKey: maskKey)
-      let data = frame.data
-      let pass = Data([0x81, 0x85, 0x39, 0xfa, 0xab, 0x35, 0x71, 0x9f, 0xc7, 0x59, 0x56])
-      XCTAssert(data == pass, "Frame does not match with text case")
+        let maskKey = Buffer([0x39, 0xfa, 0xab, 0x35])
+        let frame = Frame(opCode: .text, data: "Hello", maskKey: maskKey)
+        let data = frame.data
+        let pass = Buffer([0x81, 0x85, 0x39, 0xfa, 0xab, 0x35, 0x71, 0x9f, 0xc7, 0x59, 0x56])
+        XCTAssert(data == pass, "Frame does not match with text case")
     }
 }
 
